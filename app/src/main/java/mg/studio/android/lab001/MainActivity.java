71885f55140e55b96b366b35d529a70691caada3
WindWaving <13833799573@163.com>
2020-03-06 20:03:38 +0800

    add landscape view and events

--- app/src/main/java/mg/studio/android/lab001/MainActivity.java
+++ app/src/main/java/mg/studio/android/lab001/MainActivity.java
@@ -2,7 +2,9 @@ package mg.studio.android.lab001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +27,43 @@ public class MainActivity extends AppCompatActivity {
         nose = findViewById(R.id.nose);
         shoes = findViewById(R.id.shoes);
     }
+    private void setView(View view){
+        if(view.getVisibility()==View.VISIBLE){
+            view.setVisibility(View.GONE);
+        }else{
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+    public void cbMouth(View view){
+        setView(mouth);
+    }
+    public void cbArms(View view){
+        setView(arms);
+    }
+    public void cbEars(View view){
+        setView(ears);
+    }
+    public void cbEyebrow(View view){
+        setView(eyebrows);
+    }
+    public void cbEye(View view){
+        setView(eyes);
+    }
+    public void cbGlasses(View view){
+        setView(glasses);
+    }
+    public void cbHat(View view){
+        setView(hat);
+    }
+    public void cbMustache(View view){
+        setView(mustache);
+    }
+    public void cbNose(View view){
+        setView(nose);
+    }
+    public void cbShoes(View view){
+        setView(shoes);
+    }
+
 
 }
